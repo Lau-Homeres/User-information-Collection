30fee7a1916c45dd189cb77cedd597ac59c30fda
Lau Homeres <laurencehomeres0@gmail.com>
2024-11-04 17:20:43 +0800

    Initial commit

---
+++ userprofile.js
@@ -0,0 +1,14 @@
+// UserProfile.js
+
+// User information
+let fullName = "Lauchan Naismith";  // replace with user's full name
+let age = 20;               // replace with user's age
+let favoriteNumber = 6;     // replace with user's favorite number
+let favoriteColor = "Yellow"; // replace with user's favorite color
+
+// Log user information to the console
+console.log("User Profile:");
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
